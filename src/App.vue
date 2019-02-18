e7db5a2b8a94f96145073b92039aaa38d98bbc78
chenhui3022 <47681122+chenhui3022@users.noreply.github.com>
2019-02-18 13:51:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <keep-alive include="Home,City">
+    <keep-alive include="Home">
       <router-view/>
     </keep-alive>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-//  created(){  //将图片存储在客户端中
-//    if(sessionStorage.getItem("galleryImgs")){
-//      this.$store.replaceState(Object.assign({},this.$store.state,JSON.parse(sessionStorage.getItem("galleryImgs"))))
-//    }
-//    window.addEventListener("beforeunload",() => {
-//      sessionStorage.setItem("galleryImgs",JSON.stringify(this.$store.state))
-//    })
-//  }
+export default {
+  name: 'App',
+  created(){  //将图片存储在客户端中
+    //页面加载时读取sessionStorage里的图片信息
+    if(sessionStorage.getItem("galleryImgs")){
+      this.$store.replaceState(Object.assign({},this.$store.state,JSON.parse(sessionStorage.getItem("galleryImgs"))))
+    }
+    //在页面刷新时将vuex中的图片信息保存到sessionStorage里
+    window.addEventListener("beforeunload",() => {
+      sessionStorage.setItem("galleryImgs",JSON.stringify(this.$store.state))
+    })
   }
+}
 </script>
 
 <style>
